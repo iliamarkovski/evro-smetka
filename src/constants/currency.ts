4871b9f19f88120e7986d993440cf5c6cc1faea0
Ilia Markovski <i.markovski@jdm.de>
2025-12-12 14:25:59 +0200

    feat: add currency constants for Euro and Bulgarian Lev

---
+++ src/constants/currency.ts
@@ -0,0 +1,6 @@
+export const EURO_TO_BGN_RATE = 1.95583;
+export const EURO_SIGN = '€';
+export const BGN_SIGN = 'лв.';
+
+// After this date, only EUR change is shown
+export const CUTOFF_DATE = new Date('2026-02-01');
